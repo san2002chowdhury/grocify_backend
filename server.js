69296c2a53eac85e2cecff2fb2892e3cec9b6b56
Santanu <sunilkrchowdhury66@gmail.com>
2026-04-08 17:56:00 +0530

    first

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 8000;
 
 dbConnect();
 
-app.use(express.json());
 
 app.use(cors({
     origin: [
@@ -26,6 +25,17 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+app.options("/*", (req, res) => {
+    res.sendStatus(200);
+});
+app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 app.use("/grocify/user", userRoute);
