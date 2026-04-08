56913dc050326a9594ba5645127bbe5e5b8e0114
Santanu <sunilkrchowdhury66@gmail.com>
2026-04-08 17:58:49 +0530

    first

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
 });
-app.options("/*", (req, res) => {
-    res.sendStatus(200);
-});
+
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
