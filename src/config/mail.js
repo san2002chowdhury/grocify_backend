d9689f644a8386a4a6a8b297751822dcb138037f
Santanu <sunilkrchowdhury66@gmail.com>
2026-07-04 18:46:20 +0530

    try fixing7

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,26 +1,23 @@
 import dotenv from "dotenv/config";
 import nodemailer from "nodemailer";
+import dns from "dns";
 
+const lookup = (hostname, options, cb) => {
+    return dns.lookup(hostname, { family: 4 }, cb);
+};
 
 export const transport = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-
     auth: {
         user: process.env.mailUser,
         pass: process.env.mailPass,
     },
-
-    family: 4
-});
-
-transport.verify((err) => {
-    if (err) {
-        console.log("VERIFY ERROR:", err);
-    } else {
-        console.log("SMTP READY");
-    }
+    tls: {
+        rejectUnauthorized: false,
+    },
+    lookup,
 });
 
 export function mailConfiguratin({ to, subject, html }) {
