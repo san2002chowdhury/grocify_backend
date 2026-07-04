25c66634fc933499d9b9f5a8152c39bbde2924b3
Santanu <sunilkrchowdhury66@gmail.com>
2026-07-04 11:14:24 +0530

    save

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,18 +1,12 @@
 import nodemailer from "nodemailer";
 
 export const transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
     service: "gmail",
     auth: {
         user: process.env.mailUser,
         pass: process.env.mailPass
-    },
-    tls: {
-        rejectUnauthorized: false
     }
-});
+})
 
 export function mailConfiguratin({ to, subject, html }) {
     return {
